683d0bbd32bdd57976ea7f18035eb807c8170918
Lars Roloff <la851rol@htwg-konstanz.de>
2023-11-19 18:29:37 +0100

    intersection impl. mit array. O(schrecklich)

--- lineSegmentIntersection.js
+++ lineSegmentIntersection.js
@@ -1,5 +1,6 @@
 let lineArr = [],
     eventArr = [],
+    intersectionArr = [],
     drawing = false,
     anchor  = {
         x: 0,
@@ -8,10 +9,59 @@ let lineArr = [],
     proposedEndpoint = {
         x: 0,
         y: 0
+    };
+
+// SWEEP LINE ALGORITHM
+function findIntersections(){
+// Initialize eventArr Q with all events sorted by x-coordinate
+    for (let line of lineArr) {
+        if (line.from.x === line.to.x){
+            eventArr.push(new Event(3, line.from.x, line))
+        }
+        else {
+            let startEvent = new Event(1, line.from.x, line)
+            let endEvent = new Event(2, line.to.x, line)
+            eventArr.push(startEvent, endEvent)
+        }
     }
+    // sort descending to pop the next smallest event by x
+    eventArr.sort((a,b) => {
+        return b.x - a.x
+    })
+    console.log(eventArr)
+
+
+    while (eventArr.length !== 0){
+        let event = eventArr.pop()
+        switch (event.type){
+            case 1: {
+                event.line.isActive = true
+                break
+            }
+            case 2: {
+                event.line.isActive = false
+                break
+            }
+            case 3: {
+                let upperBound = Math.max(event.line.to.y, event.line.from.y)
+                let lowerBound = Math.min(event.line.to.y, event.line.from.y)
+                for (const line of lineArr) {
+                    if (line.isActive && line.to.y <= upperBound &&
+                        line.to.y >= lowerBound){
+                        intersectionArr.push(new Intersection(event.x, line.from.y))
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+// EVERYTHING ELSE
 
 class Line{
-    constructor(fromX, fromY, toX, toY) {
+    constructor(fromX, fromY, toX, toY, isActive) {
         this.from = {
             x: fromX,
             y: fromY
@@ -20,6 +70,7 @@ class Line{
             x: toX,
             y: toY
         }
+        this.isActive = isActive
     }
     draw(){
         ctx.strokeStyle = "white"
@@ -37,9 +88,29 @@ class Line{
 }
 
 class Event{
-    constructor(type, x) {
-        this.type = type
+    constructor(type, x, line) {
+        this.type = type //1, 2, 3 (start, end, intersection)
         this.x = x
+        this.line = line
+    }
+}
+
+class Intersection{
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+    draw(){
+        ctx.fillStyle = "yellow"
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+}
+
+function drawIntersections(){
+    for (const intersection of intersectionArr) {
+        intersection.draw()
     }
 }
 
@@ -51,7 +122,7 @@ function drawLines(){
 function aim(){
     let angleToMouse = Math.atan2(mouse.y - anchor.y, mouse.x - anchor.x)
     let radius = Math.floor(Math.sqrt(Math.pow(mouse.x - anchor.x, 2)+ Math.pow(mouse.y - anchor.y, 2)))
-    console.log(angleToMouse)
+    // console.log(angleToMouse)
     let pi = Math.PI
     let degrees = Math.floor(angleToMouse / pi * -180)
 
@@ -132,6 +203,7 @@ function aim(){
 }
 function handleIntersections(){
     drawLines()
+    drawIntersections()
     if (drawing) aim()
 }
 
@@ -141,12 +213,23 @@ function mouseClickIntersect() {
         anchor.x = mouse.x
         anchor.y = mouse.y
     }
-    else lineArr.push(new Line(anchor.x, anchor.y, proposedEndpoint.x, proposedEndpoint.y))
+    else {
+        // make sure lines always go from left to right
+        lineArr.push(new Line(
+            Math.min(anchor.x, proposedEndpoint.x),
+            anchor.y,
+            Math.max(proposedEndpoint.x, anchor.x),
+            proposedEndpoint.y,
+            false,))
+        findIntersections()
+    }
     drawing = !drawing
 }
 
 function resetLines(){
     lineArr.length = 0
+    eventArr.length = 0
+    intersectionArr.length = 0
 }
 function resetEvents(){
     eventArr.length = 0
